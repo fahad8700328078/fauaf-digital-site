2eebd521dace06ccc9dc4dfbfcb91f8123b5cc56
fahad8700328078 <146562937+fahad8700328078@users.noreply.github.com>
2025-07-26 02:45:12 +0530

    Create page.jsx

---
+++ app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <section>
+      <h2>About FAUAF DIGITAL</h2>
+      <p>We are a creative digital agency focused on building beautiful and functional digital experiences. From websites to branding, we do it all.</p>
+    </section>
+  );
+}
