b62273a3cb31798be2925906116ff8b4be39d032
fahad8700328078 <146562937+fahad8700328078@users.noreply.github.com>
2025-07-26 02:11:50 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
+      <h1>Welcome to FAUAF DIGITAL</h1>
+      <p>Your professional digital partner.</p>
+    </div>
+  );
+}
