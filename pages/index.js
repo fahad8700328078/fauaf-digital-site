fc313afc6718e813029e92d45ae9e11ee2ae034b
fahad8700328078 <146562937+fahad8700328078@users.noreply.github.com>
2025-07-26 02:37:35 +0530

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
-      <h1>Welcome to FAUAF DIGITAL</h1>
-      <p>Your professional digital partner.</p>
-    </div>
-  );
-}
